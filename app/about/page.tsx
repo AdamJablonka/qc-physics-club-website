a612671b272752c8674de8b7353f41e136b64047
Carlos Vega <cvthegreat@Carloss-MacBook-Pro.local>
2023-09-20 21:40:03 -0400

    Temp commit

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,8 +1,14 @@
+"use client";
+import Image from "next/image";
+
+
+
+
 export default function Events() {
 
   return (
-    <div>
-      About
+    <div className="center-photo">
+      <Image src="/images/gang.JPEG" alt="Photo of the developers of the website"/>
     </div>
   )
 }
