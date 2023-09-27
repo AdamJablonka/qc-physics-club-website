e1d8d98ee4226e302a42eb4d28e3ad3dc82f9b4d
AdamJablonka <ajablonka@clearblade.com>
2023-09-26 20:35:52 -0400

    implemented carousel

---
+++ app/components/ImageCarousel.tsx
@@ -0,0 +1,89 @@
+"use client";
+import React from "react";
+import "react-responsive-carousel/lib/styles/carousel.min.css";
+import { Box, Container } from "@mui/material";
+import { Swiper, SwiperSlide } from "swiper/react";
+import SwiperCore, {
+  EffectCoverflow,
+  Pagination,
+  Navigation,
+  Autoplay,
+} from "swiper";
+import "swiper/components/pagination/pagination.min.css";
+import "swiper/components/effect-coverflow/effect-coverflow.min.css";
+import "swiper/components/navigation/navigation.min.css";
+import "swiper/swiper.scss";
+import Image from "next/image";
+import { useTheme } from "@mui/material/styles";
+
+SwiperCore.use([EffectCoverflow, Pagination, Navigation, Autoplay]);
+
+interface ImageData {
+  src: string;
+  alt: string;
+  legend?: string;
+  width?: number;
+  height?: number;
+}
+
+interface ImageCarouselProps {
+  images: ImageData[];
+}
+
+export const ImageCarousel = ({ images }: ImageCarouselProps) => {
+  const theme = useTheme();
+
+  return (
+    <Box>
+      <Swiper
+        style={
+          {
+            width: "90vw",
+            paddingBottom: "2rem",
+          } as React.CSSProperties
+        }
+        autoplay={{
+          delay: 2500,
+          disableOnInteraction: false,
+        }}
+        effect="coverflow"
+        navigation
+        grabCursor={true}
+        centeredSlides={true}
+        coverflowEffect={{
+          rotate: 50,
+          stretch: 0,
+          depth: 100,
+          modifier: 1,
+          slideShadows: true,
+        }}
+        breakpoints={{
+          640: {
+            slidesPerView: 1.25,
+            spaceBetween: 10,
+          },
+          320: {
+            slidesPerView: 1.25,
+            spaceBetween: 0.5,
+          },
+        }}
+        pagination={{
+          clickable: true,
+          dynamicBullets: true,
+        }}
+      >
+        {images.map((image, index) => (
+          <SwiperSlide key={index}>
+            <Image
+              src={image.src}
+              alt={image.alt}
+              width={image.width || 1280}
+              height={image.height || 720}
+              layout="responsive"
+            />
+          </SwiperSlide>
+        ))}
+      </Swiper>
+    </Box>
+  );
+};
