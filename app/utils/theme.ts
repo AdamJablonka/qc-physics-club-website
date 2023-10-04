9083b7be6583be33a0c1ec339fedf993d155639d
AdamJablonka <ajablonka@clearblade.com>
2023-10-03 20:13:39 -0400

    background color

--- app/utils/theme.ts
+++ app/utils/theme.ts
@@ -4,5 +4,9 @@ import { createTheme } from "@mui/material";
 export const theme = createTheme({
   palette: {
     mode: "dark",
+    background: {
+      default: "#222222",
+      paper: "#242526",
+    },
   },
 });
