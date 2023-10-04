2565e22233b817f7b2f35f4a43cb2f59bac8f85b
AdamJablonka <ajablonka@clearblade.com>
2023-10-03 22:32:39 -0400

    added changed to events page; added calender to events page; events page use their index as an id

--- app/events/[slug]/page.tsx
+++ app/events/[slug]/page.tsx
@@ -1,10 +1,56 @@
-export default function productPage({ params }: { params: { slug: string } }) {
-    // Convert the slug back to a title for display (optional)
-    const title = params.slug.replace(/-/g, ' ').toUpperCase();
+"use client";
+import { Box, Container, Typography } from "@mui/material";
+import { eventsData } from "../utils";
+import React from "react";
+import Calendar from "react-calendar";
+import "react-calendar/dist/Calendar.css";
+import { styled } from "@mui/material/styles";
 
-    return (
-        <div>
-            Here is the specific {title} page.
-        </div>
-    )
+const DarkStyledCalendar = styled(Calendar)(({ theme }) => ({
+  ...(theme.palette.mode === "dark" && {
+    background: "#424242 !important",
+    color: "#e0e0e0 !important",
+    "& .react-calendar__tile--active": {
+      background: "#536dfe !important",
+      color: "#ffffff !important",
+    },
+    "& .react-calendar__navigation button, & .react-calendar__month-view__days__day--weekend":
+      {
+        color: "#bbdefb !important",
+      },
+    "& .react-calendar__tile--now": {
+      background: "#1e88e5 !important",
+      color: "#ffffff !important",
+    },
+  }),
+}));
+
+export default function productPage({ params }: { params: { slug: string } }) {
+  const event = eventsData[Number(params.slug)];
+
+  return (
+    <Container>
+      <Box
+        display="flex"
+        flexDirection="column"
+        alignItems="center"
+        justifyContent="center"
+        marginTop="2rem"
+        marginBottom="2rem"
+      >
+        <Typography variant="h2" align="center" gutterBottom>
+          {event.title}
+        </Typography>
+        <Typography variant="h6" align="center">
+          {event.description}
+        </Typography>
+        <DarkStyledCalendar
+          navigationLabel={() => null}
+          value={event.date}
+          next2Label={null}
+          prev2Label={null}
+        />
+      </Box>
+    </Container>
+  );
 }
---
+++ app/events/utils/imageData.tsx
@@ -0,0 +1,23 @@
+export const imageData = [
+  {
+    src: "/images/IMG_3167.jpg",
+    alt: "image1",
+    legend: "Image 1",
+    width: 1280,
+    height: 720,
+  },
+  {
+    src: "/images/IMG_3167.jpg",
+    alt: "image2",
+    legend: "Image 2",
+    width: 1280,
+    height: 720,
+  },
+  {
+    src: "/images/IMG_3167.jpg",
+    alt: "image2",
+    legend: "Image 3",
+    width: 1280,
+    height: 720,
+  },
+];
